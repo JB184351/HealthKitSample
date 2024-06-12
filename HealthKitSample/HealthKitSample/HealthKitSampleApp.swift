5f2da390e19db8d8b31d93a57eb0fd6b621f3b98
JB184351 <justinbengtson@icloud.com>
2024-06-12 15:27:43 -0700

    Created new Xcode project.

---
+++ HealthKitSample/HealthKitSample/HealthKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitSampleApp.swift
+//  HealthKitSample
+//
+//  Created by Justin on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
