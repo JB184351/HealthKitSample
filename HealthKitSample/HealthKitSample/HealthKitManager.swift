305235f4d242f40e91251f5e0cd8bfb0c04ef5e3
JB184351 <justinbengtson@icloud.com>
2024-06-18 12:08:50 -0700

    step bar chart styling

--- HealthKitSample/HealthKitSample/HealthKitManager.swift
+++ HealthKitSample/HealthKitSample/HealthKitManager.swift
@@ -27,9 +27,13 @@ import Observation
         let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.stepCount), predicate: queryPredicate)
         let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate, options: .cumulativeSum, anchorDate: endDate, intervalComponents: .init(day: 1))
         
-        let stepcounts = try! await stepsQuery.result(for: store)
-        stepData = stepcounts.statistics().map {
-            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        do {
+            let stepcounts = try await stepsQuery.result(for: store)
+            stepData = stepcounts.statistics().map {
+                .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
@@ -43,10 +47,13 @@ import Observation
         let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.bodyMass), predicate: queryPredicate)
         let weightQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate, options: .mostRecent, anchorDate: endDate, intervalComponents: .init(day: 1))
         
-        let weights = try! await weightQuery.result(for: store)
-        
-        weightData = weights.statistics().map {
-            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+        do {
+            let weights = try await weightQuery.result(for: store)
+            weightData = weights.statistics().map {
+                .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+            }
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
