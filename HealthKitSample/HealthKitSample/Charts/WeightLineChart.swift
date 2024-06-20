bbf571b358730a5ab3587d31e84dae133205d9aa
JB184351 <justinbengtson@icloud.com>
2024-06-20 15:14:35 -0700

    weight line chart styling

--- HealthKitSample/HealthKitSample/Charts/WeightLineChart.swift
+++ HealthKitSample/HealthKitSample/Charts/WeightLineChart.swift
@@ -14,16 +14,8 @@ struct WeightLineChart: View {
     var selectedStat: HealthMetricContext
     var chartData: [HealthMetric]
     
-    var selectedHealthMetric: HealthMetric? {
-        guard let rawSelectedDate else { return nil }
-        return chartData.first { Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date )}
-    }
-    
-    var avgStepCount: Double {
-        guard !chartData.isEmpty else { return 0 }
-        let totalSteps = chartData.reduce(0) { $0 + $1.value }
-        
-        return totalSteps / Double(chartData.count)
+    var minValue: Double {
+        chartData.map { $0.value }.min() ?? 0
     }
     
     var body: some View {
@@ -48,14 +40,40 @@ struct WeightLineChart: View {
             .padding(.bottom, 12)
             
             Chart {
+                RuleMark(y: .value("Goal", 155))
+                    .foregroundStyle(.mint)
+                    .lineStyle(.init(lineWidth: 1, dash: [5]))
+                
                 ForEach(chartData) { weight in
-                    AreaMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
-                        .foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
+                    
+                    AreaMark(
+                             x: .value("Day", weight.date, unit: .day),
+                             yStart: .value("Value", weight.value),
+                             yEnd: .value("Min Value", minValue)
+                    )
+                    .foregroundStyle(Gradient(colors: [.indigo.opacity(0.5), .clear]))
                     
                     LineMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
+                        .foregroundStyle(.indigo)
+                        .interpolationMethod(.catmullRom)
+                        .symbol(.circle)
                 }
             }
             .frame(height: 150)
+            .chartYScale(domain: .automatic(includesZero: false))
+            .chartXAxis {
+                AxisMarks {
+                    AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+                }
+            }
+            .chartYAxis {
+                AxisMarks { value in
+                    AxisGridLine()
+                        .foregroundStyle(Color.secondary.opacity(0.3))
+                    
+                    AxisValueLabel()
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
