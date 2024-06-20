2ef9943f4b30edbf43236562c9a37a2a6e6e486d
JB184351 <justinbengtson@icloud.com>
2024-06-20 16:13:34 -0700

    calculating weight differences

--- HealthKitSample/HealthKitSample/Charts/ChartMath.swift
+++ HealthKitSample/HealthKitSample/Charts/ChartMath.swift
@@ -26,4 +26,31 @@ struct ChartMath {
         
         return weekdayCharData
     }
+    
+    static func averageDailyWeightsDiffs(for weights: [HealthMetric]) -> [WeekdayChartData] {
+        var diffValues: [(date: Date, value: Double)] = []
+        
+        for i in 1..<weights.count {
+            let date = weights[i].date
+            let diff = weights[i].value - weights[i - 1].value
+            diffValues.append((date: date, value: diff))
+        }
+        
+        let sortedByWeekday = diffValues.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
+        let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
+        
+        var weekdayCharData: [WeekdayChartData] = []
+        
+        for array in weekdayArray {
+            guard let firstValue = array.first else { continue }
+            let total = array.reduce(0) { $0 + $1.value }
+            let averageWeightDiffs = total/Double(array.count)
+            
+            weekdayCharData.append(.init(date: firstValue.date, value: averageWeightDiffs))
+        }
+        
+        
+        
+        return weekdayCharData
+    }
 }
